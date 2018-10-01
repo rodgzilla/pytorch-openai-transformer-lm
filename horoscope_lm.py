bd4993364a98f2c88a6138c3b8e5c7f654354704
Grégory Châtel <chatel.gregory@gmail.com>
2018-10-01 15:44:23 +0200

    Data loading code.

--- horoscope_lm.py
+++ horoscope_lm.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import pdb
 import argparse
+import itertools
 
 import numpy as np
 
@@ -13,24 +14,43 @@ from utils import encode_dataset, flatten, iter_data, ResultLogger, make_path
 from text_utils import TextEncoder
 from datasets import rocstories
 
-def load_dataset(path = 'data/horoscope_dataset.csv', shuffle = True,
-                 seed = 42):
-    df = pd.read_csv(path)
-    all_text = ' '.join(df.TEXT)
-    word_list = all_text.split(' ')
+def _chunk_word_list(word_list, max_sequence_len = 50000):
     # We have to split the text into text of 100.000 characters
     # because of the parser limitations.
-    word_sequences = [[]]
+    word_sequences    = [[]]
     last_sequence_len = 0
-    max_sequence_len = 50000
     for word in word_list:
+        # If the last word list has reached the maximum size
         if last_sequence_len + len(word) > max_sequence_len:
+            # We transform it into a string by rejoining the words
+            word_sequences[-1] = ' '.join(word_sequences[-1])
+            # and then begin a new word sequence
             word_sequences.append([])
             last_sequence_len = 0
         word_sequences[-1].append(word)
         last_sequence_len += len(word)
 
+    if type(word_sequences[-1]) == list:
+        word_sequences[-1] = ' '.join(word_sequences[-1])
+
     return word_sequences
+
+def load_dataset(text_encoder, path = 'data/horoscope_dataset.csv',
+                 window_size = 150, shuffle = True, seed = 42):
+    df             = pd.read_csv(path)
+    all_text       = ' '.join(df.TEXT)
+    word_list      = all_text.split(' ')
+    word_sequences = _chunk_word_list(word_list, )
+    encoded_text   = text_encoder.encode(word_sequences)
+    word_idx_list  = list(itertools.chain.from_iterable(encoded_text))
+    dataset        = []
+
+    for start_idx in range(len(word_idx_list) - window_size - 1):
+        context = word_idx_list[start_idx : start_idx + window_size]
+        target  = word_idx_list[start_idx + window_size]
+        dataset.append((context, target))
+
+    return dataset
 
 def try_on_a_sentence(model, lm_head, text_encoder, sentence, n_ctx):
     # pdb.set_trace()
@@ -92,7 +112,7 @@ def main(sentence, max_size):
 
     return model
 
-bpe_path       = 'model/vocab_40000.bpe'
-encoder_path   = 'model/encoder_bpe_40000.json'
-text_encoder   = TextEncoder(encoder_path, bpe_path)
-word_sequences = load_dataset()
+bpe_path     = 'model/vocab_40000.bpe'
+encoder_path = 'model/encoder_bpe_40000.json'
+text_encoder = TextEncoder(encoder_path, bpe_path)
+dataset      = load_dataset(text_encoder)
